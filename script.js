78353b0a70f9f4abc3715bafdcb777496b8660f3
Emmett <62869287+emmett-white@users.noreply.github.com>
2020-09-10 05:02:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+/*                 */
+/*   Typing anim   */
+/*                 */
+
+new TypeIt("#loading", {
+    speed: 50,
+    waitUntilVisible: true
+  })
+    .type("Loading, plrase", {delay: 400})
+    .move(-3)
+    .delete(1)
+    .type('e')
+    .move('END')
+    .type(' wait...')
+    .go();
+
+
+/*                 */
+/*   Display form  */
+/*                 */
+
+setTimeout(async () => {
+    document.getElementById('loading').style.display = 'none'
+    document.querySelector('form').style.display = 'block'
+}, 4000)
